042af337351fec2dc9500a8159073e363a1d6c1a
Isaac Oliveira <isaacoli@gmail.com>
2018-02-15 23:36:11 -0200

    setup host name for diferent enviroments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  SERVER_HOST = "http://shortfish.herokuapp.com/"
 end
